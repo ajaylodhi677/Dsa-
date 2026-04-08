1098ef3affa58f493d55d178bbb61a276a40eaf6
ajaylodhi677 <ajaylodhi677.com>
2026-04-08 19:30:48 +0530

    max and min in array

---
+++ src/dsa_sheet_array/hello1.java
@@ -0,0 +1,36 @@
+package dsa_sheet_array;
+
+import java.util.Scanner;
+
+public class hello1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter size of array");
+        int size = sc.nextInt();
+
+        int arr[] =new int[size];
+        for(int i =0;i< arr.length;i++){
+            arr[i]= sc.nextInt();
+        }
+        for (int num:arr){
+            System.out.print(num+" ");
+        }
+        int max =arr[0];
+        int min =arr[0];
+
+            for(int i =1;i< arr.length;i++){
+                if(arr[i]>max){
+                    max = arr[i];
+                }
+                if(arr[i]<min){
+                    min =arr[i] ;
+                }
+            }
+        System.out.println();
+        System.out.println("max ="+max);
+        System.out.println("min ="+min);
+
+    }
+
+}
+
