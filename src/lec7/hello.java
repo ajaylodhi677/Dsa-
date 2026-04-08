c8c420c9c3adad30b4756ffb87bb2b6bbaf76fb4
ajaylodhi677 <ajaylodhi677.com>
2026-04-08 13:57:42 +0530

    added

--- src/lec7/hello.java
+++ src/lec7/hello.java
@@ -2,13 +2,30 @@ package lec7;
 
 public class hello {
     public static void merge(int []arr,int low ,int mid,int h){
-        int temp []=new int[h-low+1];
+        int temp[]=new int[h-low+1];
         int i = low;
         int j =mid+1;
-        int k =
+        int k = 0;
+
+        while(i<=mid && j<=h){
+            if(arr[i]<=arr[j]){
+                temp[k++]=arr[i++];
+            }else{
+                temp[k++]=arr[j++];
+            }
+        }
+        while (i<=mid){
+            temp[k++] =arr[i++];
+        }
+        while(j<=h){
+            temp[k++]=arr[j++];
+        }
+        for(int x=0;x<temp.length;x++){
+            arr[low+x]=temp[x];
+        }
     }
     public static void mergeSort(int arr[],int low,int h){
-        if(low<=h){
+        if(low>=h){
             return;
         }
         int mid =(low+h)/2;
@@ -18,7 +35,10 @@ public class hello {
 
     }
     public static void main(String[] args) {
-        int [] arr ={6,2,9,4,6 };
+        int [] arr ={5,2,9,4,6 };
         mergeSort(arr,0,arr.length-1);
+        for(int x: arr){
+        System.out.println(x);
+        }
     }
 }
